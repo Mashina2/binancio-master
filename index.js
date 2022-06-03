c538b769706af73746ca17b8463556a8a29cf0b7
Nikhil Kharana <60419615+Nikkrana28@users.noreply.github.com>
2022-06-03 18:15:23 +0530

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const interview = require("./src/interview.js");
+const axios = require('axios')
 
 function run() {
   setInterval(async()=>{
     const data = await interview()
     console.log("Price",data)
-  },3000);
+    axios.post('https://price.loca.lt/price',{
+          "price":data[0].toString()
+        })
+            .then(function (response) {
+              console.log("mmkprice",response);
+    
+            })
+          .catch(function (error) {
+          console.log("Error",error);
+        });
+    console.log("running")
+  },3600000);
 }
 
 run();
--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ require("./conn")
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+    });
+
 
 app.get("/", (req, res) =>{
     res.send("Hello from server")
